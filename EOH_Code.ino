456349480e15d6d6118c5c6b095aa5ebd87c928d
cnieves3 <80369026+cnieves3@users.noreply.github.com>
2021-03-10 18:46:32 -0600

    Update EOH_Code.ino
    Initial Code for Testing Servo Motors with Fingers

--- EOH_Code.ino
+++ EOH_Code.ino
@@ -1,12 +1,4 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
+// Initial Arduino Code Used for Servo Motor Testing
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
