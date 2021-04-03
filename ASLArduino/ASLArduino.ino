68ba62a0d1b829272b13b33d664f30de3e079d75
cfp32 <80604520+cfp32@users.noreply.github.com>
2021-04-03 18:01:03 -0500

    Update ASLArduino.ino

--- ASLArduino/ASLArduino.ino
+++ ASLArduino/ASLArduino.ino
@@ -1,10 +1,21 @@
 #include "ASLHeaders.h"
-
+finger thumb(0,1);
 
 // the loop function runs over and over again forever
 void loop() {
+  
+  if(Serial.available()){
+      char  input = Serial.read();
+      Serial.print("You typed: ");
+      Serial.println(input);
+      thumb.setFinger(input);
+
+
+  }
+  char alph;
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
+
